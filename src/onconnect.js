94c93f24a50854bab21f2c9197ce8494ff682ba7
myanzik <myanz.470shrestha@gmail.com>
2023-10-18 11:29:52 +0545

    added lambdas

---
+++ src/onconnect.js
@@ -0,0 +1,22 @@
+
+const AWS = require('aws-sdk');
+
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
+
+exports.handler = async event => {
+  const putParams = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      connectionId: event.requestContext.connectionId,
+      ttl: parseInt((Date.now() / 1000) + 3600)
+    }
+  };
+
+  try {
+    await ddb.put(putParams).promise();
+  } catch (err) {
+    return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) };
+  }
+
+  return { statusCode: 200, body: 'Connected.' };
+};
